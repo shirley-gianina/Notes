2e1834a930e457038a1ebae6f02ce58f814a4809
Shirley Gianina <shirley.cd853f@gmail.com>
2022-01-08 22:49:07 +0100

    heroku

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const routes = require("./config/routes");
-const cors = require('cors')
+const cors = require("cors");
 
 require("./config/db.config");
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 app.use("/", routes);
-
 
 const PORT = process.env.PORT || 3000;
 
